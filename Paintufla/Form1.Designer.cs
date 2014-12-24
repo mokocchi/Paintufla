c9961b470e3e1632b160a1fa07155646b4dec55a
Josefina Arcidiacono <josefine2_94@hotmail.com>
2014-12-24 04:22:32 -0300

    cambiado a .net 3.5 (andando)

--- Paintufla/Form1.Designer.cs
+++ Paintufla/Form1.Designer.cs
@@ -1,6 +1,10 @@
-﻿namespace Paintufla
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+namespace Paintufla
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Variable del diseñador requerida.
@@ -29,8 +33,339 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nuevoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarComoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ediciónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.verToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.imagenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.coloresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.labelColorAct = new System.Windows.Forms.Panel();
+            this.tiraColores = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel10 = new System.Windows.Forms.Panel();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.tiraColores.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.ediciónToolStripMenuItem,
+            this.verToolStripMenuItem,
+            this.imagenToolStripMenuItem,
+            this.coloresToolStripMenuItem,
+            this.ayudaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(742, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nuevoToolStripMenuItem,
+            this.abrirToolStripMenuItem,
+            this.guardarToolStripMenuItem,
+            this.guardarComoToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // nuevoToolStripMenuItem
+            // 
+            this.nuevoToolStripMenuItem.Name = "nuevoToolStripMenuItem";
+            this.nuevoToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.nuevoToolStripMenuItem.Text = "Nuevo";
+            // 
+            // abrirToolStripMenuItem
+            // 
+            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.abrirToolStripMenuItem.Text = "Abrir";
+            // 
+            // guardarToolStripMenuItem
+            // 
+            this.guardarToolStripMenuItem.Name = "guardarToolStripMenuItem";
+            this.guardarToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.guardarToolStripMenuItem.Text = "Guardar";
+            // 
+            // guardarComoToolStripMenuItem
+            // 
+            this.guardarComoToolStripMenuItem.Name = "guardarComoToolStripMenuItem";
+            this.guardarComoToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.guardarComoToolStripMenuItem.Text = "Guardar Como";
+            // 
+            // ediciónToolStripMenuItem
+            // 
+            this.ediciónToolStripMenuItem.Name = "ediciónToolStripMenuItem";
+            this.ediciónToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
+            this.ediciónToolStripMenuItem.Text = "Edición";
+            // 
+            // verToolStripMenuItem
+            // 
+            this.verToolStripMenuItem.Name = "verToolStripMenuItem";
+            this.verToolStripMenuItem.Size = new System.Drawing.Size(36, 20);
+            this.verToolStripMenuItem.Text = "Ver";
+            // 
+            // imagenToolStripMenuItem
+            // 
+            this.imagenToolStripMenuItem.Name = "imagenToolStripMenuItem";
+            this.imagenToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.imagenToolStripMenuItem.Text = "Imagen";
+            // 
+            // coloresToolStripMenuItem
+            // 
+            this.coloresToolStripMenuItem.Name = "coloresToolStripMenuItem";
+            this.coloresToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.coloresToolStripMenuItem.Text = "Colores";
+            // 
+            // ayudaToolStripMenuItem
+            // 
+            this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
+            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.ayudaToolStripMenuItem.Text = "Ayuda";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Location = new System.Drawing.Point(10, 33);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(722, 290);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PictureBox1MouseDown);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PictureBox1MouseUp);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 5;
+            this.timer1.Tick += new System.EventHandler(this.Timer1Tick);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(99, 19);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(51, 21);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(410, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(65, 24);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Borrar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.labelColorAct);
+            this.groupBox1.Controls.Add(this.tiraColores);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.groupBox1.Location = new System.Drawing.Point(10, 329);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(722, 50);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Cosas";
+            // 
+            // labelColorAct
+            // 
+            this.labelColorAct.BackColor = System.Drawing.Color.Red;
+            this.labelColorAct.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelColorAct.Location = new System.Drawing.Point(359, 21);
+            this.labelColorAct.Name = "labelColorAct";
+            this.labelColorAct.Size = new System.Drawing.Size(18, 18);
+            this.labelColorAct.TabIndex = 17;
+            // 
+            // tiraColores
+            // 
+            this.tiraColores.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tiraColores.Controls.Add(this.panel2);
+            this.tiraColores.Controls.Add(this.panel10);
+            this.tiraColores.Controls.Add(this.panel9);
+            this.tiraColores.Controls.Add(this.panel8);
+            this.tiraColores.Controls.Add(this.panel7);
+            this.tiraColores.Controls.Add(this.panel6);
+            this.tiraColores.Controls.Add(this.panel5);
+            this.tiraColores.Controls.Add(this.panel4);
+            this.tiraColores.Controls.Add(this.panel3);
+            this.tiraColores.Controls.Add(this.panel1);
+            this.tiraColores.Location = new System.Drawing.Point(170, 20);
+            this.tiraColores.Name = "tiraColores";
+            this.tiraColores.Size = new System.Drawing.Size(182, 20);
+            this.tiraColores.TabIndex = 8;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Location = new System.Drawing.Point(162, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(18, 18);
+            this.panel2.TabIndex = 17;
+            this.panel2.Tag = "";
+            this.panel2.Click += new System.EventHandler(this.Panel1Click);
+            this.panel2.DoubleClick += new System.EventHandler(this.Panel2DoubleClick);
+            // 
+            // panel10
+            // 
+            this.panel10.BackColor = System.Drawing.Color.White;
+            this.panel10.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel10.Location = new System.Drawing.Point(144, 0);
+            this.panel10.Name = "panel10";
+            this.panel10.Size = new System.Drawing.Size(18, 18);
+            this.panel10.TabIndex = 16;
+            this.panel10.Click += new System.EventHandler(this.Panel1Click);
+            // 
+            // panel9
+            // 
+            this.panel9.BackColor = System.Drawing.Color.Black;
+            this.panel9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel9.Location = new System.Drawing.Point(126, 0);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(18, 18);
+            this.panel9.TabIndex = 15;
+            this.panel9.Click += new System.EventHandler(this.Panel1Click);
+            // 
+            // panel8
+            // 
+            this.panel8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.panel8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel8.Location = new System.Drawing.Point(108, 0);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(18, 18);
+            this.panel8.TabIndex = 14;
+            this.panel8.Click += new System.EventHandler(this.Panel1Click);
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.Fuchsia;
+            this.panel7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel7.Location = new System.Drawing.Point(90, 0);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(18, 18);
+            this.panel7.TabIndex = 13;
+            this.panel7.Click += new System.EventHandler(this.Panel1Click);
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.Blue;
+            this.panel6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel6.Location = new System.Drawing.Point(72, 0);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(18, 18);
+            this.panel6.TabIndex = 12;
+            this.panel6.Click += new System.EventHandler(this.Panel1Click);
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.Cyan;
+            this.panel5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel5.Location = new System.Drawing.Point(54, 0);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(18, 18);
+            this.panel5.TabIndex = 11;
+            this.panel5.Click += new System.EventHandler(this.Panel1Click);
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Lime;
+            this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel4.Location = new System.Drawing.Point(36, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(18, 18);
+            this.panel4.TabIndex = 10;
+            this.panel4.Click += new System.EventHandler(this.Panel1Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Yellow;
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Location = new System.Drawing.Point(18, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(18, 18);
+            this.panel3.TabIndex = 9;
+            this.panel3.Click += new System.EventHandler(this.Panel1Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Red;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(18, 18);
+            this.panel1.TabIndex = 8;
+            this.panel1.Click += new System.EventHandler(this.Panel1Click);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(19, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 21);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Ancho Pincel";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MainForm
+            // 
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(742, 388);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.KeyPreview = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "Paint";
+            this.Load += new System.EventHandler(this.MainFormLoad);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainFormKeyDown);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.tiraColores.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
--- Paintufla/Form1.cs
+++ Paintufla/Form1.cs
@@ -1,20 +1,167 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Paintufla
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+
+        private ToolStripMenuItem abrirToolStripMenuItem;
+        private int ancho = 2;
+        private ToolStripMenuItem archivoToolStripMenuItem;
+        private ToolStripMenuItem ayudaToolStripMenuItem;
+        private Button button1;
+        private Color colorActual = Color.Black;
+        private ColorDialog colorDialog1;
+        private ToolStripMenuItem coloresToolStripMenuItem;
+        private ComboBox comboBox1;
+        private ToolStripMenuItem ediciónToolStripMenuItem;
+        private GroupBox groupBox1;
+        private ToolStripMenuItem guardarComoToolStripMenuItem;
+        private ToolStripMenuItem guardarToolStripMenuItem;
+        private ToolStripMenuItem imagenToolStripMenuItem;
+        private Label label2;
+        private Panel labelColorAct;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem nuevoToolStripMenuItem;
+        private Panel panel1;
+        private Panel panel10;
+        private Panel panel2;
+        private Panel panel3;
+        private Panel panel4;
+        private Panel panel5;
+        private Panel panel6;
+        private Panel panel7;
+        private Panel panel8;
+        private Panel panel9;
+        private PictureBox pictureBox1;
+        private bool pinta = false;
+        private Timer timer1;
+        private Panel tiraColores;
+        private ToolStripMenuItem verToolStripMenuItem;
+        private int x = 20;
+        private int y = 20;
+
+        // Methods
+        public MainForm()
         {
-            InitializeComponent();
+            this.InitializeComponent();
+        }
+
+        private void Button1Click(object sender, EventArgs e)
+        {
+            this.pictureBox1.Image.Dispose();
+            this.pictureBox1.Image = new Bitmap(this.pictureBox1.Width, this.pictureBox1.Height);
+        }
+
+        private bool enRango(int pX, int pY)
+        {
+            if (pX < 0)
+            {
+                return false;
+            }
+            if (pX >= this.pictureBox1.Width)
+            {
+                return false;
+            }
+            if (pY < 0)
+            {
+                return false;
+            }
+            if (pY >= this.pictureBox1.Height)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private void MainFormKeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyData)
+            {
+                case Keys.Add:
+                    this.ancho += 2;
+                    break;
+
+                case Keys.Subtract:
+                    if (this.ancho >= 0)
+                    {
+                        this.ancho -= 2;
+                    }
+                    break;
+
+                case Keys.Oemplus:
+                    this.ancho += 2;
+                    break;
+
+                case Keys.OemMinus:
+                    if (this.ancho >= 0)
+                    {
+                        this.ancho -= 2;
+                    }
+                    break;
+            }
+        }
+
+        private void MainFormLoad(object sender, EventArgs e)
+        {
+            this.pictureBox1.Image = new Bitmap(this.pictureBox1.Width, this.pictureBox1.Height);
+            for (int i = 0; i <= 40; i++)
+            {
+                this.comboBox1.Items.Add(i);
+            }
+            this.comboBox1.SelectedIndex = 4;
+            this.labelColorAct.BackColor = Color.Black;
+        }
+
+        private void Panel1Click(object sender, EventArgs e)
+        {
+            if ((sender as Panel).BackgroundImage == null)
+            {
+                this.colorActual = (sender as Panel).BackColor;
+                this.labelColorAct.BackColor = (sender as Panel).BackColor;
+            }
+        }
+
+        private void Panel2DoubleClick(object sender, EventArgs e)
+        {
+            if (this.colorDialog1.ShowDialog() != DialogResult.Cancel)
+            {
+                this.panel2.BackgroundImage = null;
+                (sender as Panel).BackColor = this.colorDialog1.Color;
+                this.colorActual = (sender as Panel).BackColor;
+                this.labelColorAct.BackColor = (sender as Panel).BackColor;
+            }
+        }
+
+        private void PictureBox1MouseDown(object sender, MouseEventArgs e)
+        {
+            this.pinta = true;
+        }
+
+        private void PictureBox1MouseUp(object sender, MouseEventArgs e)
+        {
+            this.pinta = false;
+        }
+
+        private void Timer1Tick(object sender, EventArgs e)
+        {
+            if (this.pinta)
+            {
+                this.x = this.pictureBox1.PointToClient(Control.MousePosition).X;
+                this.y = this.pictureBox1.PointToClient(Control.MousePosition).Y;
+                for (int i = 0; i < (this.ancho * this.ancho); i++)
+                {
+                    int pX = (this.x - (this.ancho / 2)) + (i / this.ancho);
+                    int pY = (this.y - (this.ancho / 2)) + (i % this.ancho);
+                    if (this.enRango(pX, pY))
+                    {
+                        (this.pictureBox1.Image as Bitmap).SetPixel(pX, pY, this.colorActual);
+                    }
+                }
+            }
+            this.pictureBox1.Refresh();
         }
     }
 }
